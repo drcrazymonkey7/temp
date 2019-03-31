3d60ad938bc1dee22bf800f0aa52ef8bbc345df3
drcrazymonkey7 <48869872+drcrazymonkey7@users.noreply.github.com>
2019-03-31 10:10:57 -0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -5,5 +5,6 @@ client = MPDClient()
 client.timeout = 200
 client.idletimeout = None
 client.connect("localhost", 6600)
+client.clear()
 client.add("spotify:album:0ETFjACtuP2ADo6LFhL6HN")
 client.play()
