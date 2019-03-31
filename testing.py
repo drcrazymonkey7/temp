f2bb52a9c9e126bae1d3d6a44a9b7896921f4aa5
drcrazymonkey7 <48869872+drcrazymonkey7@users.noreply.github.com>
2019-03-31 08:16:10 -0700

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from mpd import MPDClient
+
+def connectMPD():
+	try:
+		client = MPDClient()               # create client object
+		client.timeout = 200               # network timeout in seconds (floats allowed), default: None
+		client.idletimeout = None
+		print("Connecting...")
+		client.connect("localhost", 6600)
+		print("Connected!")
+		return client
+	except:
+		print('Could not connect to MPD server')
+
+def play(client, plist):
+	try:
+		client.stop()
+		client.clear()
+		client.add(plist)
+		if re.search('playlist',plist):
+			client.shuffle()
+		client.play()
+	except:
+		print('Could not play playlist %s' % plist)
+
+play("client, spotify:album:0ETFjACtuP2ADo6LFhL6HN")
+
